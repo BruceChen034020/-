4fa1e614ce739298e232641db975b019267e7173
jerrypvnrt <my072814638@gmail.com>
2021-06-23 17:57:51 +0800

    otischung modify coding style

--- replace.c
+++ replace.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    FILE *A; // 神內精神麻醉藥物.txt
-    FILE *B; // output file
-    int i; // iterator
-    char buffer; // the currently reading character
-    int f; // return of fscanf
+int main() {
+    FILE *A;      // 神內精神麻醉藥物.txt
+    FILE *B;      // output file
+    int i;        // iterator
+    char buffer;  // the currently reading character
+    int f;        // return of fscanf
 
     A = fopen("神內精神麻醉藥物.txt", "r");
     B = fopen("output.txt", "w");
-    for(i=0; i<1000000; i++){
+    for (i = 0; i < 1000000; i++) {
         f = fscanf(A, "%c", &buffer);
-        if(f != 1){
+        if (f != 1) {
             break;
         }
-        if(buffer == ','){
+        if (buffer == ',') {
             fscanf(A, "%c", &buffer);
-            if(buffer == ' '){
+            if (buffer == ' ') {
                 fprintf(B, "%c", '\n');
-            }else{
+            } else {
                 fprintf(B, "%c", buffer);
             }
-        }else{
+        } else {
             fprintf(B, "%c", buffer);
         }
     }
